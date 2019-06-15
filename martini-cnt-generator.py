b94b9fa28cfcab7aab7a048586885909b54bba92
Martin Vögele <martinvoegele1989@gmail.com>
2019-06-15 16:26:30 +0200

    ready for python 3

--- martini-cnt-generator.py
+++ martini-cnt-generator.py
@@ -15,9 +15,9 @@ numfunce = int(sys.argv[4])    # Number of different-bead rings at the end
 if len(sys.argv)>5:
 	functype = sys.argv[5] # Type of different bead
 
-print "------------------------------------------------------------------------------"
-print "Generating a Martini model for an open CNT using "+str(numrings)+" rings with "+str(ringsize)+" each."
-print "------------------------------------------------------------------------------"
+print( "------------------------------------------------------------------------------" )
+print( "Generating a Martini model for an open CNT using "+str(numrings)+" rings with "+str(ringsize)+" each." )
+print( "------------------------------------------------------------------------------" )
 
 numatoms = numrings*ringsize
 a 	 = 0.47
@@ -27,10 +27,10 @@ R 	 = a/(2*sin(alpha/2))
 
 if numfuncb+numfunce > 0:
 	if len(sys.argv)<=5:
-		print "ERROR: You seemt o want fuctional groups but did not specify a bead type!"
+		print( "ERROR: You seemt o want fuctional groups but did not specify a bead type!" )
 		sys.exit()
 	else:
-		print "The "+str(numfuncb)+" first and the "+str(numfunce)+" last rings will be of type "+functype+"."
+		print( "The "+str(numfuncb)+" first and the "+str(numfunce)+" last rings will be of type "+functype+"." )
 		filename = "cnt-"+str(numrings)+"-"+str(ringsize)+"-f"+str(numfuncb)+str(numfunce)+"-"+functype
 else:
 	filename = "cnt-"+str(numrings)+"-"+str(ringsize)+"-f"+str(numfuncb)+str(numfunce)
@@ -57,8 +57,8 @@ structure_file.write( "  %3d\n" % (numatoms) )
 
 # Atoms
 
-for i in xrange(0, numrings):
-	for j in xrange(0, ringsize):
+for i in range(0, numrings):
+	for j in range(0, ringsize):
 		n = i*ringsize + j + 1
 		p = j*alpha + i%2*alpha/2
 		x = R*sin(p)
@@ -115,9 +115,9 @@ else:
 topology_file.write( "\n[ atoms ]\n" )
 topology_file.write( "; nr	 type	 resnr	 residue	 atom	 cgnr	 charge	 mass\n" )
 
-#for i in xrange(1, numatoms+1):
-for m in xrange(0, numrings):
-	for n in xrange(1, ringsize+1):
+#for i in range(1, numatoms+1):
+for m in range(0, numrings):
+	for n in range(1, ringsize+1):
 		i = m*ringsize + n
 		if ( m < numfuncb or m >= numrings-numfunce):
 			topology_file.write( "%3d    %4s   1   CNT    F%03d     %3d       0      48\n" % (i, functype, i, i) )
@@ -132,8 +132,8 @@ topology_file.write( "; i	 j	  funct	 length	 force\n" )
 
 topology_file.write( "; rings\n" )
 
-for m in xrange(1, numrings+1):
-	for n in xrange(1, ringsize+1):
+for m in range(1, numrings+1):
+	for n in range(1, ringsize+1):
 		i = (m-1)*ringsize + n
 		j = (m-1)*ringsize + n%ringsize + 1
 		topology_file.write( "     %3d     %3d       1   0.470    5000\n" % (i, j) )
@@ -141,8 +141,8 @@ for m in xrange(1, numrings+1):
 
 topology_file.write( "; between rings, short\n" )
 
-for m in xrange(1, numrings):
-	for n in xrange(1, ringsize+1):
+for m in range(1, numrings):
+	for n in range(1, ringsize+1):
 		# odd-numbered rings
 		if m%2 == 1: 	
 			i = (m-1)*ringsize + n
@@ -172,8 +172,8 @@ topology_file.write( "; i	 j	 k	 funct	 angle	 force\n" )
 
 topology_file.write( "; in rings\n" )
 
-for m in xrange(1, numrings+1):
-	for n in xrange(1, ringsize+1):
+for m in range(1, numrings+1):
+	for n in range(1, ringsize+1):
 		i = (m-1)*ringsize + n
 		j = (m-1)*ringsize + n%ringsize + 1
 		k = (m-1)*ringsize + (n+1)%ringsize + 1
@@ -186,8 +186,8 @@ topology_file.write( "\n[ dihedrals ]\n" )
 topology_file.write( "; i	 j	 k	 l     func	 q0     cq\n" )
 
 
-for m in xrange(1, numrings-1):
-	for n in xrange(1, ringsize+1):
+for m in range(1, numrings-1):
+	for n in range(1, ringsize+1):
 		i = (m-1)*ringsize + n
 		j = m*ringsize + n
 		l = (m+1)*ringsize + n
@@ -195,7 +195,7 @@ for m in xrange(1, numrings-1):
 		if m%2 == 1:
 			if i%ringsize == 1: 
 				k = (m+1)*ringsize
-                        else:
+			else:
 				k = m*ringsize + (n-1)
 			topology_file.write( "     %3d     %3d     %3d     %3d       2        %3.3f      350\n" % (i,k,j,l,beta) )
 		# even-numbered rings
@@ -233,7 +233,7 @@ posres_file = open(filename+"-posres.itp", 'w')
 posres_file.write( "[ position_restraints ]\n" )
 posres_file.write( "; ai  funct  fcx    fcy    fcz\n" )
 
-for i in xrange(1, numatoms+1):
+for i in range(1, numatoms+1):
 	posres_file.write( " %3d    1    1000   1000   1000\n" % (i) )
 	
 posres_file.close()
