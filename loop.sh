8e298647894b3ed4871b35420942fc61e0537a9c
Martin Vögele <martin.voegele@biophys.mpg.de>
2019-06-24 17:16:31 +0200

    updated example loop

--- loop.sh
+++ loop.sh
@@ -1,7 +1,7 @@
 for NUMRINGS in 08 10 12 14 16 18 20 24 28 ; do
 	for RINGSIZE in 08 10 12 14 16 20 30 40 ; do
 		for NUMFUNC in 0 1 2 ; do
-			python martini-cnt-generator.py -nr $NUMRINGS -rs $RINGSIZE -ft SNda -nb $NUMFUNC -ne $NUMFUNC
+			python martini-cnt-generator.py -nr $NUMRINGS -rs $RINGSIZE -ft SNda -fb $NUMFUNC -fe $NUMFUNC
 		done
 	done
 done
