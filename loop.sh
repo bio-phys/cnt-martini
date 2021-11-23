5e97d55bb68f857306a9758dbd3f9d5a100bb10b
martinvoegele <martinvoegele1989@gmail.com>
2021-11-23 13:00:14 -0800

    add force constant options

--- loop.sh
+++ loop.sh
@@ -1,7 +1,7 @@
 for NUMRINGS in 08 10 12 14 16 18 20 24 28 ; do
 	for RINGSIZE in 08 10 12 14 16 20 30 40 ; do
 		for NUMFUNC in 0 1 2 ; do
-			python martini-cnt-generator.py -nr $NUMRINGS -rs $RINGSIZE -ft SNda -fb $NUMFUNC -fe $NUMFUNC
+			python martini-cnt-generator.py -nr $NUMRINGS -rs $RINGSIZE -ft SNda -fb $NUMFUNC -fe $NUMFUNC -bf 20000
 		done
 	done
 done
